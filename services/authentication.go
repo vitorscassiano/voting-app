ffa89fea6e441ece7fc3b084f94ebd84844eef2f
vitor.cassiano <vitor.cassiano@picpay.com>
2022-04-26 09:01:04 -0300

    updates

--- services/authentication.go
+++ services/authentication.go
@@ -1,29 +1,33 @@
 package services
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/vitorscassiano/voting-app/database"
 	"github.com/vitorscassiano/voting-app/wires"
 	"golang.org/x/crypto/bcrypt"
 )
 
-/*
-func Authorize(email string, password string) bool {
-	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		fmt.Println("something wrong with hashing password")
-	}
-
-	string(hashPassword)
-
-	return true
-}
-*/
+// This value will be regenerated and moved to configurations
+const SECRET_KEY = "Uv38ByGCZU8WP18PmmIdcpVmx00QA3xNe7sEB9HixkmBhVrYaB0NhtHpHgAWeTnLZpTSxCKs0gigByk5SH9pmQ=="
 
 func userAuthenticated(hashPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+}
+
+func generateJWTToken(userID string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	claims["userId"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(SECRET_KEY))
 }
 
 func Authorize(ctx *gin.Context) {
@@ -40,5 +44,9 @@ func Authorize(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"success": "OK"})
+	jwtString, err := generateJWTToken(user.UserID)
+	if err != nil {
+		fmt.Println("something wrong with jwt generation", err.Error())
+	}
+	ctx.JSON(http.StatusOK, gin.H{"token": jwtString})
 }
